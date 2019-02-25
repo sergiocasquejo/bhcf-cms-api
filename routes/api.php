346d4371dbe8d1070fdf90dc6e867e673a45374c
Serge <serg.casquejo@gmail.com>
2019-02-25 16:40:51 +0800

    Remove extra spaces

--- routes/api.php
+++ routes/api.php
@@ -14,19 +14,16 @@ use Illuminate\Http\Request;
 Route::prefix('v1')->group(function() {
     Route::post('login', ['as' => 'login', 'uses' => 'Auth\LoginController@login']);
     Route::post('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
-
     Route::group(['middleware' => 'auth:api'], function() {
         Route::resource('roles', 'RoleController')->only(['store', 'show', 'update', 'destroy']);
         Route::resource('auxiliary-groups', 'AuxiliaryGroupController')->only(['index', 'store', 'show', 'update', 'destroy']);
         Route::resource('ministries', 'MinistryController')->only(['index', 'store', 'show', 'update', 'destroy']);
         Route::resource('statuses', 'StatusController')->only(['index', 'store', 'show', 'update', 'destroy']);
         Route::resource('leadership-level', 'LeadershipLevelController')->only(['index', 'store', 'show', 'update', 'destroy']);
-
         Route::resource('members/category', 'MemberCategoryController')->only(['index', 'store', 'show', 'update', 'destroy']);
         Route::post('members/{id}/avatar', 'MemberController@uploadAvatar');
         Route::resource('members', 'MemberController')->only(['index', 'store', 'show', 'update', 'destroy']);
         Route::resource('users', 'UserController')->only(['store', 'show', 'update', 'destroy']);
-        
         Route::resource('sc-statuses', 'SundayCelebrationStatusController')->only(['index', 'store', 'show', 'update', 'destroy']);
         Route::prefix('school')->group(function () {
             Route::resource('classes', 'SchoolClassController')->only(['index', 'store', 'show', 'update', 'destroy']);
@@ -36,10 +33,6 @@ Route::prefix('v1')->group(function() {
             Route::resource('statuses', 'SchoolStatusController')->only(['index', 'store', 'show', 'update', 'destroy']);
             Route::resource('types', 'SchoolTypeController')->only(['index', 'store', 'show', 'update', 'destroy']);
         });
-        
-        
     });
-    
-    
 });
 
