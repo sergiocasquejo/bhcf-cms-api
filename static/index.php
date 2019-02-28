40001ade8d20341d43c3a71fdfea12ed5c2d087e
lm-marvin <marvin@lightmedia.com.au>
2019-02-28 21:18:36 +0800

    first

---
+++ static/index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <title>Hello, world!</title>
+    <link rel="stylesheet" href="dist/styles/main.css">
+</head>
+<body>
+<div class="admin-interface">
+    <div class="container admin-interface-container">
+        <div class="admin-interface-block">
+            <div class="admin-interface-sidebar">
+                <img class="navbar-logo" src="/dist/images/bhcf-logo.png" alt="">
+                <ul>
+                    <li class="active"><a href=""><i class="icon-home"></i>Dashboard</a></li>
+                    <li><a href=""><i class="icon-team"></i>People</a></li>
+                    <li><a href=""><i class="icon-affiliate"></i>Cell Group</a></li>
+                    <li><a href=""><i class="icon-writing"></i>Registrations</a></li>
+                    <li><a href=""><i class="icon-like"></i>Ministries</a></li>
+                    <li><a href=""><i class="icon-event"></i>Events</a></li>
+                    <li><a href=""><i class="icon-prayer"></i>Services</a></li>
+                    <li><a href=""><i class="icon-plant"></i>Finance</a></li>
+                    <li><a href=""><i class="icon-ebook"></i>Booking</a></li>
+                    <li><a href=""><i class="icon-trello"></i>Boards</a></li>
+                    <li><a href=""><i class="icon-signal-bars"></i>Reports</a></li>
+                </ul>
+            </div>
+            <div class="admin-interface-main">
+                <div class="admin-interface-main-header">
+                    <div class="admin-interface-main-container">
+                        <ul>
+                            <li>
+                                <form>
+                                    <div class="form-group">
+                                        <button type="submit" class="btn btn-link"><i class="icon-search"></i></button>
+                                        <input type="text" class="form-control" id="sr" aria-describedby="emailHelp" placeholder="Search">
+                                    </div>
+                                </form>
+                            </li>
+                            <li><a href=""><i class="icon-musical-bell-outline"></i></a></li>
+                            <li><a href=""><img src="/dist/images/user-img.jpg" alt="">Pastor Jhong <i class="icon-down-arrow"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="admin-interface-main-body">
+                    <div class="admin-interface-main-container">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<script src="dist/scripts/jquery.js"></script>
+<script src="dist/scripts/main.js"></script>
+</body>
+</html>
