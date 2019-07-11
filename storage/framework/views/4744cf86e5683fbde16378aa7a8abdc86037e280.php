8b98d78fda16cfb02ff324d39cba07da6bf677a7
Serge <serg.casquejo@gmail.com>
2019-07-11 23:18:28 +0800

    reinit .gitignore

--- storage/framework/views/4744cf86e5683fbde16378aa7a8abdc86037e280.php
+++
@@ -1,6 +0,0 @@
-# Info
-
-Welcome to the generated API reference.
-<?php if($showPostmanCollectionButton): ?>
-[Get Postman Collection](<?php echo e(url($outputPath.'/collection.json')); ?>)
-<?php endif; ?>
